5fb3cda6c7acf73be183bec50947548fb7ab08fb
PaulIvanDimacali <pauldimacali05@gmail.com>
2026-01-12 02:42:55 +0800

    Added the Layout of the Website

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,34 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { act, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+    <Nav />
+    <Hero />
+    <Projects />
+    <Skills />
+    <Skill_list />
+
     </>
+  )
+}
+
+function Nav() {
+  return ( 
+          <div className="nav">
+        <a href = ""> About </a>
+        <a href = ""> Skills </a>
+        <a href = ""> Projects </a>
+        <a href = ""> Contact </a>
+      </div>
+  )
+}
+
+function Hero() {
+  return (
+          <div className="hero">
+        <h1>Paul's Portfolio</h1>
+      </div>
+  )
+}
+
+function Projects() {
+  return (
+    <div className="projects">
+      <h2>Projects</h2>
+    </div>
+  )
+}
+
+function Skills() {
+  return (
+    <div className="skills">
+      <h2>Skills</h2>
+    </div>
+  )
+}
+
+function Skill_list() {
+  const [active, setActive] = useState(null);
+
+  if (active) {
+    console.log(active);
+  }
+  return (
+    <div className="grid">
+      <div className="skill-list">
+        <button id="backend" className="square" onClick={() => setActive("backend")}></button>
+          {active === "backend" && <div id="backend_tools" className="tools">bold</div>}
+        </div>
+
+        <div className="skill-list">
+          <button id="webdev" className="square" onClick={() => setActive("webdev")}></button>
+          {active === "webdev" && <div id="webdev_tools" className="tools">ni</div>}
+        </div>
+
+        <div className="skill-list">
+          <button id="tools" className="square" onClick={() => setActive("tools")}></button>
+          {active === "tools" && <div id="tools_tools" className="tools">wally</div>}
+        </div>
+      </div>
   )
 }
 
